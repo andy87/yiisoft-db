bd6451832e28c79ab90d523f1cc9fcefa125e966
and_y87 <kidin_a@list.ru>
2019-11-01 02:52:31 +0300

    test

--- mapper.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use andy87\yii2\common\models\DataBase;
-use andy87\yii2\console\controllers\DbController;
-
-class_alias(DataBase::class, 'andy87\yii2\common\models');
-class_alias(DbController::class, 'andy87\yii2\console\controllers');
