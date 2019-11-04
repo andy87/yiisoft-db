ad747a5cb27dc3f9d4aef79b1f85987025fa580c
and_y87 <kidin_a@list.ru>
2019-11-04 18:36:39 +0300

    Add TPL dir + files

---
+++ src/_tpl/console/controllers/DbControllers.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace console\controllers;
+
+use andy87\yii2\db\console\controllers\DbController as Source;
+/**
+ * Class DbController
+ *
+ *      Controller
+ *
+ * @package console\controllers
+ */
+class DbController extends Source
+{
+    // ...
+}
